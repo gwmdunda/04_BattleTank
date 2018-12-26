3b827a616829bca868e47b07255f87e1cf9ec53b
gwmdunda <gerry_dunda@yahoo.com>
2018-12-26 10:22:45 +0800

    just adding quit button

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	FVector ForceApplied = GetForwardVector()*Throttle*TrackMaxDrivingForce;
+	FVector ForceLocation = GetComponentLocation();
+	UPrimitiveComponent* TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied,ForceLocation);
+}
